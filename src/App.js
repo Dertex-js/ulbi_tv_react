4ff44c4179a22f34fe234c73745699600959caf3
Dertex-js <ryoamir60@gmail.com>
2022-05-05 13:58:04 +0500

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, {useState} from "react";
+import Counter from "./components/Counter";
+import ClassCounter from "./components/ClassCounter";
+
+function App() {
+    const [value, setValue] = useState("ТЕКСТ В ИНПУТЕ")
+
+  return (
+    <div className="App">
+        <ClassCounter/>
+        <h1>{value}</h1>
+        <input
+            type="text"
+            value={value}
+            onChange={event => setValue(event.target.value)}
+        />
+    </div>
+  );
+}
+
+export default App;
